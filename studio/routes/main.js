d404f0d181b0d015cb1e6926792be36b9880b2a3
scpex <mr.nathakit@live.com>
2019-06-12 07:37:21 +0000

    update harperdb studio

--- studio/routes/main.js
+++
@@ -1,9 +0,0 @@
-const express = require("express"),
-  router = express.Router(),
-  isAuthenticated = require("../utility/checkAuthenticate").isAuthenticated;
-
-router.get("/", isAuthenticated, function(req, res) {
-  res.render("index", { user: req.user });
-});
-
-module.exports = router;
